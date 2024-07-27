beb5b73cc3bb307a5f5896ebd7a56e75ac8be15d
KadeSan4uk <kotejchik@gmail.com>
2024-07-28 01:24:40 +0300

    BackGround, Player and PlayerController.

--- Assets/Player/Scripts/PLayerController.cs
+++ Assets/Player/Scripts/PLayerController.cs
@@ -1,18 +1,75 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class PLayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rb;
+    private float HorizontalMove = 0f;
+    private bool FacingRight = true;
+
+    [Header("Player Movement Settings")]
+    [Range(0, 10f)] public float Speed = 1f;
+    [Range(0, 15f)] public float JumpForce = 8f;
+
+    [Space]
+    [Header("Ground Checer Settings")]
+    public bool IsGrounded = false;
+    [Range(-5f, 5f)] public float CheckGroundOffsetY = -1.8f;
+    [Range(0, 5f)] public float CheckGroundRadius = 0.3f;
+
+
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        JumpPlayer();
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * Speed;
+
+        FlipSprite();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(HorizontalMove * 10f, _rb.velocity.y);
+        _rb.velocity = targetVelocity;
+    }
+
+    private void Flip()
+    {
+        FacingRight = !FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void FlipSprite()
+    {
+        if (HorizontalMove < 0 && FacingRight)
+            Flip();
+        else if (HorizontalMove > 0 && !FacingRight)
+            Flip();
+    }
+    private void JumpPlayer()
+    {
+        CheckGround();
+        if (IsGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(transform.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll
+            (new Vector2(transform.position.x, transform.position.y + CheckGroundOffsetY), CheckGroundRadius);
+
+        if (colliders.Length > 0)
+            IsGrounded = true;
+        else
+            IsGrounded = false;
     }
 }
